249620af4f1cfe5ff00650c4730aae5c1e9fd6ce
Les Pounder <lespounder@gmail.com>
2019-02-17 15:37:31 +0000

    Added easygui

--- Inspiratron.py
+++ Inspiratron.py
@@ -1,21 +1,28 @@
 from googlesearch import search
 from random import choice
 import webbrowser
+import easygui as eg
 
 
-print("Tell me three things that you would like in a project")
+#print("Tell me three things that you would like in a project")
+eg.msgbox(msg="Tell me what you would like in a project and I shall search Google for you. This will trigger 3 pop up windows", title="InPIration", ok_button="Inspire Me!")
 things = []
 suggestion = []
 
 for i in range(3):
-    things.append(input("READY >>> "))
+    #things.append(input("READY >>> "))
+    things.append(eg.enterbox(msg="READY >>> "))
 
 inspiration = str(things[0]+" "+things[1]+" "+things[2])
 print(inspiration) #Debug
-print("Hey I found these that might be of interest, I'll open one at random too")
 
 for item in search(inspiration, tld="co.uk", num=3, stop=1, pause=2):
     print(item)
     suggestion.append(item)
 
+for i in range(len(suggestion)):
+    links = "\n".join(suggestion[1:])
+    
+eg.textbox(msg="Hey I found these that might be of interest, I'll open one at random after you close this message", text=links)
+    
 webbrowser.open(choice(suggestion))
